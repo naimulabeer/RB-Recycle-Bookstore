ad97be2d88e621426bd3bcbf2e501fe340f47047
abeer117 <nabeer161117@bscse.uiu.ac.bd>
2022-12-29 20:23:28 +0600

    added tables of products in products.php

--- admin/products.php
+++ admin/products.php
@@ -14,4 +14,33 @@ include 'admin.php';
 		or die("Can not execute query");    
 ?>
 
+<div class="page-content p-5" id="content">
+    <h1 class="display-xs text-white" style="font-size: large;">Product List</h1>
+<table class="table table-hover table-dark">
+  <thead>
+    <tr>
+      <th scope="col">PID</th>
+      <th scope="col">Title</th>
+      <th scope="col">Author</th>
+      <th scope="col">Price</th>
+      <th scope="col">Available</th>
+      <th scope="col">Category</th>
+    </tr>
+  </thead>
+</div>
+<?php
+	while( $rows = mysqli_fetch_array( $results ) ) {       //everytime i call fetch array it will call first
+		extract( $rows );
+		echo "<tr>";
+		echo "<td> $PID </td>";
+		echo "<td> $Title </td>";
+        echo "<td> $Author </td>";
+        echo "<td> $Price </td>";
+        echo "<td> $Available </td>";
+        echo "<td> $Category </td>";
+		echo "</tr> \n";
+	}
+
+	echo "</table> \n";
+?>
 
